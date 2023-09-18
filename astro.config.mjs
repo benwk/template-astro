aa907f50fa9d51beff257727539bbfd9a5d702f7
Ben Z <z@benwk.com>
2023-09-18 12:13:47 +0800

    2 feature integrations (#5)
    * feat: init partytown
    
    * feat: init prefetch
    
    * feat: init compress
    
    * feat: init astro icon
    
    * feat: init webmanifest
    
    * refactor: workflows
    
    * fix: make unit test work
    
    * fix: webmanifest config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
-
 import robotsTxt from "astro-robots-txt";
+import partytown from "@astrojs/partytown";
+import prefetch from "@astrojs/prefetch";
+import compress from "astro-compress";
+import webmanifest from "astro-webmanifest";
 
 // https://astro.build/config
 export default defineConfig({
@@ -47,6 +50,39 @@ export default defineConfig({
           cleanParam: "ref /articles/",
         },
       ],
+    }),
+    partytown({
+      config: {
+        debug: false,
+        forward: ["dataLayer.push"],
+      },
+    }),
+    prefetch({
+      throttle: 3,
+    }),
+    compress({
+      Logger: 0,
+    }),
+    webmanifest({
+      name: "Ben's Astro Template",
+      icon: "src/icons/manifest.svg",
+      short_name: "Ben Z.",
+      description:
+        "Programming is my language, cmd + c and cmd + v are my lifeblood.",
+      theme_color: "#0246BC",
+      background_color: "#13151a",
+      display: "standalone",
+      lang: "en-US",
+      start_url: "/",
+      locales: {
+        "zh-cn": {
+          name: "Ben's Astro 模板",
+          short_name: "Ben Z.",
+          description: "编程才是我的母语，cmd + c 和 cmd + v 是我的命脉。",
+          lang: "zh-CN",
+          start_url: "/zh-cn",
+        },
+      },
     }),
   ],
 });
